b5cf61db06c6fc7e7a8af1c198676b044222f242
paolo.franchi <franchi.paolo94@gmail.com>
2025-11-11 14:50:53 +0100

    2025 main event quest 6

--- src/main/java/events/y2025/EC2025Quest06.java
+++ src/main/java/events/y2025/EC2025Quest06.java
@@ -1,36 +1,283 @@
 package events.y2025;
 
+import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.SortedSetMultimap;
 import common.AbstractQuest;
-import common.support.interfaces.Quest06;
+import common.Strings;
 import common.support.interfaces.MainEvent2025;
+import common.support.interfaces.Quest06;
 import common.support.params.ExecutionParameters;
+import common.support.params.GenericExecutionParameter;
 
-import java.util.List;
+import java.util.*;
 
 public class EC2025Quest06 extends AbstractQuest implements MainEvent2025, Quest06 {
 
     @Override
     protected String solvePart1(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int numberOfMentorsSeen = 0;
+        int count = 0;
 
-        return NOT_IMPLEMENTED;
+        for (char c: Strings.firstRow(input).toCharArray()) {
+            if (c == 'A') {
+                numberOfMentorsSeen++;
+            }
+            if (c == 'a') {
+                count += numberOfMentorsSeen;
+            }
+        }
+
+        return Integer.toString(count);
     }
 
     @Override
     protected String solvePart2(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        Map<Character, Long> mentorCounts = new HashMap<>();
+        long count = 0;
 
-        return NOT_IMPLEMENTED;
+        for (char c: Strings.firstRow(input).toCharArray()) {
+
+            if (Character.isUpperCase(c)) {
+                mentorCounts.merge(c, 1L, (oldValue, _) -> oldValue + 1);
+            } else {
+                count += mentorCounts.getOrDefault(Character.toUpperCase(c), 0L);
+            }
+
+        }
+
+        return Long.toString(count);
     }
+
+    record Part3Parameters(int distanceLimit, int patternRepeats) {}
 
     @Override
     protected String solvePart3(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int distanceLimit = 1000;
+        int patternRepeats = 1000;
 
-        return NOT_IMPLEMENTED;
+        if (executionParameters instanceof GenericExecutionParameter<?>(Part3Parameters(int limit, int repeats))) {
+            distanceLimit = limit;
+            patternRepeats = repeats;
+        }
+
+        char[] chars = Strings.firstRow(input).toCharArray();
+        long patternLength = chars.length;
+
+        /*
+            TODO Create only one implementation that always works
+
+            Solution 2 does not work if distanceLimit > patternLength because it is possible that the view wraps around the pattern more than once.
+            However the number of times that the view wraps around not only depends on the length of the overshoot but is also limited by the amount
+            of repetitions actually available to the side. For example, if distanceLimit = 100 and patternLength = 2, a view would wrap around
+            the pattern 50 times to the left and 50 times to the right. If there are 6 pattern repetitions, neither side can offer enough pattern
+            repetitions for the view to include a full 50 pattern repetitions. If there are 4000 pattern repetition, "starting" and "middle" repetitions
+            have enough pattern repetitions to the right to fully wrap around them, however the 3998th repetition does not have the full view to the right.
+
+            If distanceLimit <= patternLength solution 2 can be used because in this case numberOfFullWrapArounds = 0, which
+            means that only one repetition on either side is used (and not used completely).
+
+            Solution 2 is much faster.
+
+         */
+
+        if (distanceLimit > patternLength) {
+            return part3Solution1(patternRepeats, patternLength, chars, distanceLimit);
+        }
+
+        return part3Solution2(patternRepeats, (int) patternLength, chars, distanceLimit);
+
+    }
+
+    private String part3Solution1(int patternRepeats, long patternLength, char[] chars, int distanceLimit) {
+
+        // First pass: mentors
+
+        SortedSetMultimap<Character, Long> mentorPositionIndexes = MultimapBuilder.hashKeys()
+                .treeSetValues().build();
+
+        for (int repetition = 0; repetition < patternRepeats; repetition++) {
+
+            for (int patternCharIndex = 0; patternCharIndex < patternLength; patternCharIndex++) {
+
+                char c = chars[patternCharIndex];
+                long globalIndex = repetition * patternLength + patternCharIndex;
+
+                if (Character.isUpperCase(c)) {
+                    mentorPositionIndexes.put(c, globalIndex);
+                }
+
+            }
+
+        }
+
+        // Second pass
+
+        long total = 0L;
+
+        for (int repetition = 0; repetition < patternRepeats; repetition++) {
+
+            for (int patternCharIndex = 0; patternCharIndex < patternLength; patternCharIndex++) {
+
+                char c = chars[patternCharIndex];
+                long globalIndex = repetition * patternLength + patternCharIndex;
+
+                if (Character.isLowerCase(c)) {
+
+                    SortedSet<Long> allMentors = mentorPositionIndexes.get(Character.toUpperCase(c));
+                    long mentorsSeen = allMentors.subSet(globalIndex - distanceLimit, globalIndex + distanceLimit + 1).size();
+                    total += mentorsSeen;
+
+                }
+
+            }
+
+        }
+
+        return Long.toString(total);
+
+    }
+
+    private String part3Solution2(int patternRepeats, int patternLength, char[] chars, int distanceLimit) {
+
+        SortedSetMultimap<Character, Integer> mentorPositionIndexes = MultimapBuilder.hashKeys()
+                .treeSetValues().build();
+
+        for (int patternCharIndex = 0; patternCharIndex < patternLength; patternCharIndex++) {
+
+            char c = chars[patternCharIndex];
+
+            if (Character.isUpperCase(c)) {
+                mentorPositionIndexes.put(c, patternCharIndex);
+            }
+
+        }
+
+        long total = 0L;
+
+        if (patternRepeats == 1) {
+
+            // No wrap around on either side
+
+            for (int index = 0; index < patternLength; index++) {
+
+                char novice = chars[index];
+
+                if (Character.isLowerCase(novice)) {
+
+                    int leftBoundaryInclusive = index - distanceLimit;
+                    int rightBoundaryExclusive = index + distanceLimit + 1;
+
+                    SortedSet<Integer> allMentors = mentorPositionIndexes.get(Character.toUpperCase(novice));
+
+                    long mentorsSeen = allMentors.subSet(leftBoundaryInclusive, rightBoundaryExclusive).size();
+
+                    total += mentorsSeen;
+
+                }
+
+            }
+
+        } else {
+
+            // Do a pass where the right view wraps around but the left view does not
+
+            for (int index = 0; index < patternLength; index++) {
+
+                char novice = chars[index];
+
+                if (Character.isLowerCase(novice)) {
+
+                    int leftBoundaryInclusive = index - distanceLimit;
+                    int rightBoundaryExclusive = index + distanceLimit + 1;
+
+                    SortedSet<Integer> allMentors = mentorPositionIndexes.get(Character.toUpperCase(novice));
+
+                    long mentorsSeen = allMentors.subSet(leftBoundaryInclusive, rightBoundaryExclusive).size();
+
+                    if (rightBoundaryExclusive > patternLength) {
+                        // Wrap around
+
+                        int overshoot = rightBoundaryExclusive - patternLength;
+                        int numberOfFullWrapArounds = overshoot / patternLength;
+                        int singleOvershoot = overshoot % patternLength;
+
+                        mentorsSeen += (long) allMentors.size() * numberOfFullWrapArounds;
+
+                        mentorsSeen += allMentors.subSet(0, singleOvershoot).size();
+                    }
+
+                    total += mentorsSeen;
+
+                }
+
+            }
+
+            // Do a pass where the left view wraps around but the right view does not
+
+            for (int index = 0; index < patternLength; index++) {
+
+                char novice = chars[index];
+
+                if (Character.isLowerCase(novice)) {
+
+                    int leftBoundaryInclusive = index - distanceLimit;
+                    int rightBoundaryExclusive = index + distanceLimit + 1;
+
+                    SortedSet<Integer> allMentors = mentorPositionIndexes.get(Character.toUpperCase(novice));
+
+                    long mentorsSeen = allMentors.subSet(leftBoundaryInclusive, rightBoundaryExclusive).size();
+
+                    if (leftBoundaryInclusive < 0) {
+                        // Wrap around
+                        mentorsSeen += allMentors.subSet((int) (leftBoundaryInclusive + patternLength), (int) patternLength).size();
+
+                    }
+
+                    total += mentorsSeen;
+
+                }
+
+            }
+
+            // Third pass, where both views wrap around
+
+            for (int index = 0; index < patternLength; index++) {
+
+                char novice = chars[index];
+
+                if (Character.isLowerCase(novice)) {
+
+                    int leftBoundaryInclusive = index - distanceLimit;
+                    int rightBoundaryExclusive = index + distanceLimit + 1;
+
+                    SortedSet<Integer> allMentors = mentorPositionIndexes.get(Character.toUpperCase(novice));
+
+                    long mentorsSeen = allMentors.subSet(leftBoundaryInclusive, rightBoundaryExclusive).size();
+
+                    if (rightBoundaryExclusive > patternLength) {
+                        // Wrap around
+                        mentorsSeen += allMentors.subSet(0, (int) (rightBoundaryExclusive - patternLength)).size();
+                    }
+
+                    if (leftBoundaryInclusive < 0) {
+                        // Wrap around
+                        mentorsSeen += allMentors.subSet((int) (leftBoundaryInclusive + patternLength), (int) patternLength).size();
+
+                    }
+
+                    total += mentorsSeen * (patternRepeats - 2);
+
+                }
+
+            }
+
+        }
+
+        return Long.toString(total);
+
     }
 
 }
--- src/test/java/events/y2025/EC2025Quest06Test.java
+++ src/test/java/events/y2025/EC2025Quest06Test.java
@@ -1,11 +1,13 @@
 package events.y2025;
 
 import common.AbstractQuestTest;
+import common.support.params.GenericExecutionParameter;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,42 +21,60 @@ class EC2025Quest06Test extends AbstractQuestTest {
     @Order(1)
     void part1Examples() {
         List<String> actualResults = solution.executeExamples(1);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("5");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(2)
     void part1() {
-        executeQuest(solution, 1, "Not implemented");
+        executeQuest(solution, 1, "153");
     }
 
     @Test
     @Order(3)
     void part2Examples() {
         List<String> actualResults = solution.executeExamples(2);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("11");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(4)
     void part2() {
-        executeQuest(solution, 2, "Not implemented");
+        executeQuest(solution, 2, "3794");
     }
 
     @Test
     @Order(5)
     void part3Examples() {
-        List<String> actualResults = solution.executeExamples(3);
-        List<String> expectedResults = List.of("Not implemented");
+
+        EC2025Quest06.Part3Parameters example1Parameters = new EC2025Quest06.Part3Parameters(
+                10, 1);
+
+        EC2025Quest06.Part3Parameters example2Parameters = new EC2025Quest06.Part3Parameters(
+                10, 2);
+
+        EC2025Quest06.Part3Parameters example3Parameters = new EC2025Quest06.Part3Parameters(
+                1000, 1000);
+
+        List<String> actualResults = new ArrayList<>();
+
+        actualResults.addAll(solution.executeExamples(3, new GenericExecutionParameter<>(example1Parameters),
+                fn -> fn.startsWith("ex-1"), List.of()));
+        actualResults.addAll(solution.executeExamples(3, new GenericExecutionParameter<>(example2Parameters),
+                fn -> fn.startsWith("ex-2"), List.of()));
+        actualResults.addAll(solution.executeExamples(3, new GenericExecutionParameter<>(example3Parameters),
+                fn -> fn.startsWith("ex-3"), List.of()));
+
+        List<String> expectedResults = List.of("34", "72", "3442321");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(6)
     void part3() {
-        executeQuest(solution, 3, "Not implemented");
+        executeQuest(solution, 3, "1668727153");
     }
 
 
