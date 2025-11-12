a7d44ebf9d2252fb2a058907ace5e2191a0de658
paolo.franchi <franchi.paolo94@gmail.com>
2025-11-13 00:57:24 +0100

    2025 main event quest 8

--- src/main/java/events/y2025/EC2025Quest08.java
+++ src/main/java/events/y2025/EC2025Quest08.java
@@ -1,36 +1,152 @@
 package events.y2025;
 
 import common.AbstractQuest;
-import common.support.interfaces.Quest08;
+import common.Strings;
 import common.support.interfaces.MainEvent2025;
+import common.support.interfaces.Quest08;
+import common.support.params.ExecutionIntParameter;
 import common.support.params.ExecutionParameters;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class EC2025Quest08 extends AbstractQuest implements MainEvent2025, Quest08 {
+
+    private record Segment(int low, int high) {
+
+        public boolean intersects(Segment other) {
+            if (this.equals(other)) {
+                return true;
+            }
+
+            if (this.low == other.low || this.low == other.high
+                || this.high == other.low || this.high == other.high) {
+                return false;
+            }
+
+            int count = 0;
+
+            if (this.low < other.low && other.low < this.high ) {
+                count++;
+            }
+
+            if (this.low < other.high && other.high < this.high ) {
+                count++;
+            }
+
+            return count == 1;
+
+        }
+
+    }
+
+    private List<Segment> createSegments(String input) {
+
+        List<Integer> numbers = Arrays.stream(Strings.firstRow(input).split(",")).map(Integer::parseInt).toList();
+
+        List<Segment> segments = new ArrayList<>();
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+
+            int n1 = numbers.get(i);
+            int n2 = numbers.get(i + 1);
+
+            segments.add(new Segment(Integer.min(n1, n2), Integer.max(n1, n2)));
+
+        }
+
+        return segments;
+
+    }
 
     @Override
     protected String solvePart1(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int numberOfNails = 32;
 
-        return NOT_IMPLEMENTED;
+        if (executionParameters instanceof ExecutionIntParameter(int value)) {
+            numberOfNails = value;
+        }
+
+        List<Segment> segments = createSegments(input);
+
+        int count = 0;
+
+        for (Segment segment: segments) {
+            if (segment.high() - segment.low() == numberOfNails / 2) {
+                count++;
+            }
+        }
+
+        return Integer.toString(count);
+
     }
 
     @Override
     protected String solvePart2(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        /*
 
-        return NOT_IMPLEMENTED;
+        // The number of nails is not actually used in part 2. If you need it for a
+        // different implementation, uncomment this code
+
+        int numberOfNails = 256;
+
+        if (executionParameters instanceof ExecutionIntParameter(int value)) {
+            numberOfNails = value;
+        }
+
+         */
+
+        List<Segment> segments = createSegments(input);
+
+        long count = 0L;
+
+        for (int s1Index = 0; s1Index < segments.size(); s1Index++ ) {
+
+            Segment s1 = segments.get(s1Index);
+
+            count += IntStream.range(0, s1Index)
+                    .mapToObj(segments::get)
+                    .filter(s1::intersects)
+                    .count();
+
+        }
+
+        return Long.toString(count);
+
     }
 
     @Override
     protected String solvePart3(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int numberOfNails = 256;
 
-        return NOT_IMPLEMENTED;
+        if (executionParameters instanceof ExecutionIntParameter(int value)) {
+            numberOfNails = value;
+        }
+
+        List<Segment> segments = createSegments(input);
+
+        long max = Long.MIN_VALUE;
+
+        for (int lower = 1; lower <= numberOfNails; lower++) {
+            for (int upper = lower + 1; upper <= numberOfNails; upper++) {
+
+                Segment testSegment = new Segment(lower, upper);
+
+                long count = segments.stream()
+                        .filter(testSegment::intersects)
+                        .count();
+
+                max = Long.max(max, count);
+
+            }
+        }
+
+        return Long.toString(max);
     }
 
 }
--- src/test/java/events/y2025/EC2025Quest08Test.java
+++ src/test/java/events/y2025/EC2025Quest08Test.java
@@ -1,6 +1,7 @@
 package events.y2025;
 
 import common.AbstractQuestTest;
+import common.support.params.ExecutionIntParameter;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -18,43 +19,43 @@ class EC2025Quest08Test extends AbstractQuestTest {
     @Test
     @Order(1)
     void part1Examples() {
-        List<String> actualResults = solution.executeExamples(1);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> actualResults = solution.executeExamples(1, new ExecutionIntParameter(8));
+        List<String> expectedResults = List.of("4");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(2)
     void part1() {
-        executeQuest(solution, 1, "Not implemented");
+        executeQuest(solution, 1, "61");
     }
 
     @Test
     @Order(3)
     void part2Examples() {
-        List<String> actualResults = solution.executeExamples(2);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> actualResults = solution.executeExamples(2, new ExecutionIntParameter(8));
+        List<String> expectedResults = List.of("21");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(4)
     void part2() {
-        executeQuest(solution, 2, "Not implemented");
+        executeQuest(solution, 2, "2921379");
     }
 
     @Test
     @Order(5)
     void part3Examples() {
-        List<String> actualResults = solution.executeExamples(3);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> actualResults = solution.executeExamples(3, new ExecutionIntParameter(8));
+        List<String> expectedResults = List.of("7");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(6)
     void part3() {
-        executeQuest(solution, 3, "Not implemented");
+        executeQuest(solution, 3, "2796");
     }
 
 
