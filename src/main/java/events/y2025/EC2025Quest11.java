5cf67f261431fc0413f094104dffbd10dfe6e19c
paolo.franchi <franchi.paolo94@gmail.com>
2025-11-18 00:54:36 +0100

    2025 main event quest 11

--- src/main/java/events/y2025/EC2025Quest11.java
+++ src/main/java/events/y2025/EC2025Quest11.java
@@ -1,36 +1,173 @@
 package events.y2025;
 
 import common.AbstractQuest;
-import common.support.interfaces.Quest11;
 import common.support.interfaces.MainEvent2025;
+import common.support.interfaces.Quest11;
 import common.support.params.ExecutionParameters;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class EC2025Quest11 extends AbstractQuest implements MainEvent2025, Quest11 {
 
     @Override
     protected String solvePart1(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int numberOfRounds = 10;
 
-        return NOT_IMPLEMENTED;
+        List<Integer> l = new ArrayList<>(inputLines.stream().map(Integer::parseInt).toList());
+
+        int size = l.size();
+
+        int round = 0;
+
+        // Phase 1
+        boolean somethingChanged;
+        do {
+
+            somethingChanged = false;
+
+            for (int i = 0; i < size - 1; i++) {
+
+                if (l.get(i) > l.get(i + 1)) {
+                    somethingChanged = true;
+                    l.set(i, l.get(i) - 1);
+                    l.set(i + 1, l.get(i + 1) + 1);
+                }
+
+            }
+
+            if (somethingChanged) {
+                round++;
+            }
+
+            if (round == numberOfRounds) {
+                return checksum(size, l);
+            }
+
+        } while (somethingChanged);
+
+        // Phase 2
+        do {
+
+            somethingChanged = false;
+
+            for (int i = 0; i < size - 1; i++) {
+
+                if (l.get(i) < l.get(i + 1)) {
+                    somethingChanged = true;
+                    l.set(i, l.get(i) + 1);
+                    l.set(i + 1, l.get(i + 1) - 1);
+                }
+
+            }
+
+            if (somethingChanged) {
+                round++;
+            }
+
+            if (round == numberOfRounds) {
+                return checksum(size, l);
+            }
+
+        } while (somethingChanged);
+
+        throw new IllegalStateException("The list was balanced before the expected number of rounds");
+    }
+
+    private String checksum(int size, List<Integer> l) {
+        long result = IntStream.range(0, size).mapToLong(index -> (long) (index + 1) * l.get(index)).sum();
+        return Long.toString(result);
     }
 
     @Override
     protected String solvePart2(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        List<Integer> l = new ArrayList<>(inputLines.stream().map(Integer::parseInt).toList());
+
+        int globalSum = l.stream().mapToInt(i -> i).sum();
+        int size = l.size();
+
+        int balancedValue = globalSum / size;
+
+        int round = 0;
+
+        // Phase 1
+        boolean somethingChanged;
+        do {
+
+            somethingChanged = false;
+
+            for (int i = 0; i < size - 1; i++) {
+
+                if (l.get(i) > l.get(i + 1)) {
+                    somethingChanged = true;
+                    l.set(i, l.get(i) - 1);
+                    l.set(i + 1, l.get(i + 1) + 1);
+                }
+
+            }
+
+            if (somethingChanged) {
+                round++;
+            }
+
+            if (isBalanced(l, balancedValue)) {
+                return Integer.toString(round);
+            }
+
+        } while (somethingChanged);
+
+        // Phase 2
+        do {
+
+            somethingChanged = false;
+
+            for (int i = 0; i < size - 1; i++) {
+
+                if (l.get(i) < l.get(i + 1)) {
+                    somethingChanged = true;
+                    l.set(i, l.get(i) + 1);
+                    l.set(i + 1, l.get(i + 1) - 1);
+                }
+
+            }
+
+            if (somethingChanged) {
+                round++;
+            }
+
+            if (isBalanced(l, balancedValue)) {
+                return Integer.toString(round);
+            }
+
+        } while (somethingChanged);
 
         return NOT_IMPLEMENTED;
+
+    }
+
+    private boolean isBalanced(List<Integer> l, int balancedValue) {
+        return l.stream().allMatch(i -> i == balancedValue);
     }
 
     @Override
     protected String solvePart3(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        // This solution assumes that the given list of numbers is sorted in increasing order. There is no move possible in phase 1.
 
-        return NOT_IMPLEMENTED;
+        List<Long> columnValues = new ArrayList<>(inputLines.stream().map(Long::parseLong).toList());
+
+        long globalSum = columnValues.stream().mapToLong(i -> i).sum();
+        int size = columnValues.size();
+
+        long balancedValue = globalSum / size;
+
+        long result = columnValues.stream().mapToLong(n -> n).filter(n -> n > balancedValue).map(n -> (n - balancedValue)).sum();
+
+        return Long.toString(result);
+
     }
 
 }
--- src/test/java/events/y2025/EC2025Quest11Test.java
+++ src/test/java/events/y2025/EC2025Quest11Test.java
@@ -19,42 +19,42 @@ class EC2025Quest11Test extends AbstractQuestTest {
     @Order(1)
     void part1Examples() {
         List<String> actualResults = solution.executeExamples(1);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("109");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(2)
     void part1() {
-        executeQuest(solution, 1, "Not implemented");
+        executeQuest(solution, 1, "324");
     }
 
     @Test
     @Order(3)
     void part2Examples() {
         List<String> actualResults = solution.executeExamples(2);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("11", "1579");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(4)
     void part2() {
-        executeQuest(solution, 2, "Not implemented");
+        executeQuest(solution, 2, "5691445");
     }
 
     @Test
     @Order(5)
     void part3Examples() {
         List<String> actualResults = solution.executeExamples(3);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("1");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(6)
     void part3() {
-        executeQuest(solution, 3, "Not implemented");
+        executeQuest(solution, 3, "139309643324122");
     }
 
 
