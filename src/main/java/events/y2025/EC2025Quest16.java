d8d916939e6bc87ab2e64866c5996c3ed66ccdb4
paolo.franchi <franchi.paolo94@gmail.com>
2025-11-25 01:11:38 +0100

    2025 main event quest 16

--- src/main/java/events/y2025/EC2025Quest16.java
+++ src/main/java/events/y2025/EC2025Quest16.java
@@ -1,36 +1,107 @@
 package events.y2025;
 
 import common.AbstractQuest;
-import common.support.interfaces.Quest16;
+import common.Strings;
 import common.support.interfaces.MainEvent2025;
+import common.support.interfaces.Quest16;
 import common.support.params.ExecutionParameters;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class EC2025Quest16 extends AbstractQuest implements MainEvent2025, Quest16 {
 
     @Override
     protected String solvePart1(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int[] values = Arrays.stream(Strings.firstRow(input).split(",")).mapToInt(Integer::parseInt).toArray();
+        int numberOfColumns = 90;
 
-        return NOT_IMPLEMENTED;
+        long total = IntStream
+                .rangeClosed(1, numberOfColumns)
+                .mapToLong(finalColumn -> Arrays.stream(values).filter(value -> finalColumn % value == 0).count())
+                .sum();
+
+        return Long.toString(total);
     }
 
     @Override
     protected String solvePart2(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int[] wallFragmentValues = Arrays.stream(Strings.firstRow(input).split(",")).mapToInt(Integer::parseInt).toArray();
+        NavigableSet<Integer> currentSpellValues = findSpells(wallFragmentValues);
 
-        return NOT_IMPLEMENTED;
+        long product = currentSpellValues
+                .stream()
+                .mapToLong(i -> i)
+                .reduce(1, (a, b) -> a*b);
+
+        return Long.toString(product);
     }
 
     @Override
     protected String solvePart3(String input, List<String> inputLines, ExecutionParameters executionParameters) {
 
-        inputLines.forEach(this::log);
+        int[] wallFragmentValues = Arrays.stream(Strings.firstRow(input).split(",")).mapToInt(Integer::parseInt).toArray();
+        NavigableSet<Integer> currentSpellValues = findSpells(wallFragmentValues);
 
-        return NOT_IMPLEMENTED;
+        BigDecimal sumOfReciprocals = BigDecimal.ZERO;
+
+        for (int spell: currentSpellValues) {
+            BigDecimal bdValue = BigDecimal.valueOf(spell);
+            BigDecimal reciprocal = BigDecimal.ONE.divide(bdValue, MathContext.DECIMAL128);
+            sumOfReciprocals = sumOfReciprocals.add(reciprocal);
+        }
+
+        BigDecimal inverseOfSumOfReciprocals = BigDecimal.ONE.divide(sumOfReciprocals, MathContext.DECIMAL128);
+
+        long numberOfAvailableBlocks = 202520252025000L;
+        BigDecimal bdNumberOfAvailableBlocks = BigDecimal.valueOf(numberOfAvailableBlocks);
+
+        long wallLength = inverseOfSumOfReciprocals.multiply(bdNumberOfAvailableBlocks).longValue();
+
+        while (numberOfUsedBlocks(currentSpellValues, wallLength) <= numberOfAvailableBlocks) {
+            wallLength++;
+        }
+
+        return Long.toString(wallLength - 1);
+
+    }
+
+    private NavigableSet<Integer> findSpells(int[] wallFragmentValues) {
+        NavigableSet<Integer> currentSpellValues = new TreeSet<>();
+
+        int wallLength = wallFragmentValues.length;
+
+        for (int wallValueIndex = 0; wallValueIndex < wallLength; wallValueIndex++) {
+
+            int number = wallValueIndex + 1;
+            int value = wallFragmentValues[wallValueIndex];
+
+            boolean isCovered = currentSpellValues
+                                        .stream()
+                                        .mapToInt(spell -> spell)
+                                        .filter(spell -> number % spell == 0)
+                                        .count()
+                                == value;
+
+            if (!isCovered) {
+                currentSpellValues.add(number);
+            }
+
+        }
+
+        return currentSpellValues;
+    }
+
+    private long numberOfUsedBlocks(Set<Integer> spells, long wallLength) {
+        long numberOfUsedBlocks = 0;
+        for (long spell: spells) {
+            numberOfUsedBlocks += wallLength / spell;
+        }
+        return numberOfUsedBlocks;
     }
 
 }
--- src/test/java/events/y2025/EC2025Quest16Test.java
+++ src/test/java/events/y2025/EC2025Quest16Test.java
@@ -19,42 +19,42 @@ class EC2025Quest16Test extends AbstractQuestTest {
     @Order(1)
     void part1Examples() {
         List<String> actualResults = solution.executeExamples(1);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("193");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(2)
     void part1() {
-        executeQuest(solution, 1, "Not implemented");
+        executeQuest(solution, 1, "206");
     }
 
     @Test
     @Order(3)
     void part2Examples() {
         List<String> actualResults = solution.executeExamples(2);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("270");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(4)
     void part2() {
-        executeQuest(solution, 2, "Not implemented");
+        executeQuest(solution, 2, "102289195008");
     }
 
     @Test
     @Order(5)
     void part3Examples() {
         List<String> actualResults = solution.executeExamples(3);
-        List<String> expectedResults = List.of("Not implemented");
+        List<String> expectedResults = List.of("94439495762954");
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
     @Order(6)
     void part3() {
-        executeQuest(solution, 3, "Not implemented");
+        executeQuest(solution, 3, "97734826694955");
     }
 
 
